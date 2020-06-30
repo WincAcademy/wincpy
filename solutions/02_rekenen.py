e97f4aa74470779bbdeef99fc8ae9c4583d74159
stfwn <s.wijnja@me.com>
2020-06-30 16:46:32 +0200

    Remove done TODO and add print statement of result

--- solutions/02_rekenen.py
+++ solutions/02_rekenen.py
@@ -3,10 +3,7 @@ aardappel = 3
 spruitje = 7
 
 totaal = prei + aardappel + spruitje
-print(totaal)
-
 gemiddelde_prijs = round((prei + aardappel + spruitje) / 3, 2)
-print(gemiddelde_prijs)
 
 aantal_prei = 2
 aantal_aardappel = 7
@@ -15,9 +12,7 @@ aantal_spruitje = 10
 totaal_prijs = aantal_prei * prei\
                + aantal_aardappel * aardappel\
                + aantal_spruitje * spruitje
-print(totaal_prijs)
 
-# Note: korting wordt nog niet gespecificeerd in de opdracht.
 korting_percentage = 30
 te_betalen_percentage = 1 - korting_percentage / 100
 
