66e2b0093b77dfd78e10956be93766c01686102a
stfwn <s.wijnja@me.com>
2020-06-30 16:37:05 +0200

    Rename scope to state

--- helpers.py
+++ helpers.py
@@ -1,16 +1,16 @@
 def exec_assignment_code(filename):
-    """Execs the code in filename and returns a dictionary with the variables
+    """ Execs the code in filename and returns a dictionary with the variables
     in scope minus builtins. """
 
     with open(filename, 'r') as fp:
         assignment_code = fp.read()
 
-    assignment_scope = {}
+    assignment_state = {}
     print('===         Output         ===')
     print('==============================')
-    exec(assignment_code, assignment_scope)
+    exec(assignment_code, assignment_state)
     print('==============================')
     print('===    Einde van output    ===\n')
 
-    del assignment_scope['__builtins__']
-    return assignment_scope
+    del assignment_state['__builtins__']
+    return assignment_state
