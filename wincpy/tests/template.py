38e7ed20297a9ba4d747b8b93b25de1e6305e835
stfwn <s.wijnja@me.com>
2020-07-08 16:43:18 +0200

    Update tests/template.py for use with modules

--- wincpy/tests/template.py
+++ wincpy/tests/template.py
@@ -3,12 +3,15 @@
 Template for writing tests. This is just a file for convenience and has no
 importance beyond it."""
 
-from wincpy.helpers import compare_states, exec_assignment_code
+from wincpy.helpers import compare_states, exec_assignment_code, get_main_abspath
 
 __winc_id__ = ''
 
 
-def run(filename):
+def run(student_module):
     result = []
+    main_abspath = get_main_abspath(student_module)
+
+    # Do stuff
 
     return result
