22d898aeb6642379e8a015aa6e8ccc539f1e850e
stfwn <s.wijnja@me.com>
2020-08-31 17:54:44 +0200

    Add start for authentication

---
+++ wincpy/starts/8fd255f5fe5e40dcb1995184eaa26116/helpers.py
@@ -0,0 +1,16 @@
+from hashlib import sha3_512
+
+
+def get_users():
+    """ Note that this way of storing usernames and passwords is nothing like
+    how it would be done in a real application, but it suffices for this
+    exercise application."""
+
+    return {
+        'Alice': 'b778a39a3663719dfc5e48c9d78431b1e45c2af9df538782bf199c189dabeac7680ada57dcec8eee91c4e3bf3bfa9af6ffde90cd1d249d1c6121d7b759a001b1',
+        'Bob': 'd07e870dbf957e794c53fe56b0940d75c6b752096f59a90aaa31b4a420593fc5c520a2f51e884c34756e2445cfe2edce86ae566d22031bd02390a18c0fe0a63b'
+    }
+
+
+def hash_password(password):
+    return sha3_512(password.encode('utf-8')).hexdigest()
---
+++ wincpy/starts/8fd255f5fe5e40dcb1995184eaa26116/main.py
@@ -0,0 +1,48 @@
+import os
+
+from flask import Flask, redirect, render_template, request, session, url_for
+
+from .helpers import get_users, hash_password
+
+app = Flask(__name__)
+
+app.secret_key = os.urandom(16)
+
+
+@app.route('/home')
+def redirect_index():
+    return redirect(url_for('index'))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Index')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route('/lon')
+def lon():
+    return render_template('lon.html', title='League of Nations')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # YOUR SOLUTION HERE
+    pass
+
+
+@app.route('/dashboard')
+def dashboard():
+    # YOUR SOLUTION HERE
+    pass
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    # YOUR SOLUTION HERE
+    pass
+
