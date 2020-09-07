c10a5b24dda0c60a1d3566c8f9d9bff9d37ac7a1
stfwn <s.wijnja@me.com>
2020-09-07 15:30:25 +0200

    Update tests for strings

--- wincpy/tests/71dd124b4a6e4d268f5973db521394ee.py
+++ wincpy/tests/71dd124b4a6e4d268f5973db521394ee.py
@@ -7,11 +7,11 @@ def run(student_module):
     result = []
     main_abspath = get_main_abspath(student_module)
     expected_state = {
-            'goal1': 35,
-            'goal2': 54,
-            'scoorders': 'Ruud Gullit, Marco van Basten',
-            'report': 'Ruud Gullit scoorde in de 35e minuut.'
-                      + '\nMarco van Basten scoorde in de 54e minuut.'}
+            'goal_0': 35,
+            'goal_1': 54,
+            'scorers': 'Ruud Gullit 35, Marco van Basten 54',
+            'report': 'Ruud Gullit scored in the 35th minute\n' +
+                      'Marco van Basten scored in the 54th minute'}
 
     _, assignment_state = exec_assignment_code(main_abspath)
     result += compare_states(expected_state, assignment_state)
@@ -19,15 +19,16 @@ def run(student_module):
     try:
         player = assignment_state['player']
     except KeyError:
-        requirement = 'Je hebt een variabele `player`.'
+        requirement = 'There is a variable `player`.'
         result.append((requirement, False))
         return result
 
     expected_state = {
-            'firstname': player[player.find(' ') + 1:] + ', ' + player[:player.find(' ')],
-            'lastname_len': len(player[player.find(' ') + 1:]),
-            'name_short': player[0] + player[player.find(' '):],
-            'chant': ((player[:player.find(' ')] + '! ') * len(player[:player.find(' ')]))[:-1]
+            'first_name': player[:player.find(' ')],
+            'last_name_len': len(player[player.find(' ') + 1:]),
+            'name_short': player[0] + '.' + player[player.find(' '):],
+            'chant': ((player[:player.find(' ')] + '! ') * len(player[:player.find(' ')]))[:-1],
+            'good_chant': True
             }
     result += compare_states(expected_state, assignment_state)
 
