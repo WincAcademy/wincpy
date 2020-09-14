92837a8c026cf89916b44813eeab790abb0f7475
stfwn <s.wijnja@me.com>
2020-09-14 14:53:09 +0200

    Update solution to follow new spec

--- wincpy/solutions/25596924dffe436da9034d43d0af6791/__init__.py
+++ wincpy/solutions/25596924dffe436da9034d43d0af6791/__init__.py
@@ -1,2 +1,4 @@
+from .main import *
+
 __winc_id__ = '25596924dffe436da9034d43d0af6791'
 __human_name__ = 'conditions'
--- wincpy/solutions/25596924dffe436da9034d43d0af6791/main.py
+++ wincpy/solutions/25596924dffe436da9034d43d0af6791/main.py
@@ -1,30 +1,33 @@
 def farm_action(weather, time_of_day, cows_need_milking, cows_location, season,
                 slurry_tank_full, grass_long):
 
+    actions = ''
     moved_cows = False
 
     if time_of_day == 'night' and weather == 'rainy' and cows_location != 'cowshed':
-        print('take cows to cowshed')
+        actions += 'take cows to cowshed\n'
     elif cows_need_milking:
         if cows_location != 'cowshed':
-            print('take cows to cowshed')
+            actions += 'take cows to cowshed\n'
             moved_cows = True
-        print('milk cows')
+        actions += 'milk cows\n'
         if moved_cows:
-            print('take cows back to pasture')
+            actions += 'take cows back to pasture\n'
     elif slurry_tank_full and weather != 'sunny' and weather != 'windy':
         if cows_location != 'cowshed':
-            print('take cows to cowshed')
+            actions += 'take cows to cowshed\n'
             moved_cows = True
-        print('fertilize pasture')
+        actions += 'fertilize pasture\n'
         if moved_cows:
-            print('take cows back to pasture')
+            actions += 'take cows back to pasture\n'
     elif grass_long and season == 'spring' and weather == 'sunny':
         if cows_location == 'pasture':
-            print('take cows to cowshed')
+            actions += 'take cows to cowshed\n'
             moved_cows = True
-        print('mow grass')
+        actions += 'mow grass\n'
         if moved_cows:
-            print('take cows back to pasture')
+            actions += 'take cows back to pasture\n'
     else:
-        print('wait')
+        actions += 'wait\n'
+
+    return actions[:-1]
