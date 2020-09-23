72fb8b73f5bcc2bc4a58834479ece74f043f9c1f
stfwn <s.wijnja@me.com>
2020-09-23 21:32:36 +0200

    Switch over to single-file module design

--- wincpy/solutions/78029e0e504a49e5b16482a7a23af58c/__init__.py
+++
@@ -1,2 +0,0 @@
-__winc_id__ = '78029e0e504a49e5b16482a7a23af58c'
-__human_name__ = 'modules'
--- wincpy/solutions/78029e0e504a49e5b16482a7a23af58c/main.py
+++ wincpy/solutions/78029e0e504a49e5b16482a7a23af58c/main.py
@@ -5,6 +5,9 @@ import math
 from datetime import datetime as dt
 import greet
 
+__winc_id__ = '78029e0e504a49e5b16482a7a23af58c'
+__human_name__ = 'modules'
+
 
 def wait(seconds):
     time.sleep(seconds)
@@ -26,8 +29,9 @@ def supergreeting_wrapper(name):
     return greet.supergreeting(name)
 
 
-print(wait(1))
-print(my_sin(5))
-print(iso_now())
-print(platform())
-print(supergreeting_wrapper('Winc'))
+if __name__ == '__main__':
+    print(wait(1))
+    print(my_sin(5))
+    print(iso_now())
+    print(platform())
+    print(supergreeting_wrapper('Winc'))
