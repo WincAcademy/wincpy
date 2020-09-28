aa20b33309bd69d9331f3b826c0bb612d3b34f84
stfwn <s.wijnja@me.com>
2020-09-28 16:53:11 +0200

    Move solution over to new format

--- wincpy/solutions/04da020dedb24d42adf41382a231b1ed/__init__.py
+++
@@ -1,2 +0,0 @@
-__winc_id__ = '04da020dedb24d42adf41382a231b1ed'
-__human_name__ = 'classes'
--- wincpy/solutions/04da020dedb24d42adf41382a231b1ed/main.py
+++ wincpy/solutions/04da020dedb24d42adf41382a231b1ed/main.py
@@ -1,3 +1,7 @@
+__winc_id__ = '04da020dedb24d42adf41382a231b1ed'
+__human_name__ = 'classes'
+
+
 class Player():
     def __init__(self, name, speed, endurance, accuracy):
         for x in [speed, endurance, accuracy]:
