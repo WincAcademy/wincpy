a2b12a52f5e70299d1bdc5fc4f2b612ee2e1fa87
stfwn <s.wijnja@me.com>
2020-10-08 17:57:37 +0200

    Add start for superpy

---
+++ wincpy/starts/a2bc36ea784242e4989deb157d527ba0/main.py
@@ -0,0 +1,17 @@
+# Imports
+import argparse
+import csv
+from datetime import date
+
+# Do not change these lines.
+__winc_id__ = 'a2bc36ea784242e4989deb157d527ba0'
+__human_name__ = 'superpy'
+
+
+# Your code below this line.
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
