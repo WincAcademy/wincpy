9fde1833d933826fc4e1ae1c710ca4d22572655f
Stefan Wijnja (stfwn) <stefan@stfwn.com>
2020-11-04 18:06:33 +0100

    Add start and solution for stds (pytest included)

---
+++ wincpy/starts/8c2e6882503c4baa9ce2e050497c3f2f/main.py
@@ -0,0 +1,17 @@
+import argparse
+
+# Leave these lines untouched
+__winc_id__ = '8c2e6882503c4baa9ce2e050497c3f2f'
+__human_name__ = 'stds'
+
+# Your code here
+def main(args):
+    pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # TODO: add your arguments here
+
+
+    args = parser.parse_args()
+    main(args)
