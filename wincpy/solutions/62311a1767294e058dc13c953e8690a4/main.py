a05a3d00412c7d175c352a10b2807ac3a1500e5c
Niels Bom <niels@nielsbom.com>
2021-02-22 15:33:05 +0100

    Grammar in variable names

--- wincpy/solutions/62311a1767294e058dc13c953e8690a4/main.py
+++ wincpy/solutions/62311a1767294e058dc13c953e8690a4/main.py
@@ -6,8 +6,8 @@ order_amount = int(order[order.find(" ") :])
 sum_total = leek_price * order_amount
 print(sum_total)
 
-broccli_price = 2.34
+broccoli_price = 2.34
 order = "broccoli 1.6"
 order_amount = float(order[order.find(" ") :])
-sum_total = round(broccli_price * order_amount, 2)
+sum_total = round(broccoli_price * order_amount, 2)
 print(str(sum_total) + "kg broccoli costs " + str(sum_total) + "e")
