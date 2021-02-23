3cce9cb9580c2c528d9cf952dcff28bc90884acc
Niels Bom <niels@nielsbom.com>
2021-02-23 13:14:53 +0100

    Made "for" starter more clear

--- wincpy/starts/c545bc87620d4ced81cbddb8a90b4a51/main.py
+++ wincpy/starts/c545bc87620d4ced81cbddb8a90b4a51/main.py
@@ -7,9 +7,11 @@ __winc_id__ = 'c545bc87620d4ced81cbddb8a90b4a51'
 __human_name__ = 'for'
 
 
-""" Your code here. """
+""" Write your functions here. """
 
 # This block is only run if this file is the entrypoint; python main.py
 # It is not run if it is imported as a module: `from main import *`
 if __name__ == '__main__':
     countries = get_countries()
+
+    """ Write the calls to your functions here. """
