b3529f89a98b972c5b53eaf4688c5546457b80a9
Stefan Wijnja (stfwn) <stefan@stfwn.com>
2021-02-24 12:39:31 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     description="Assists students in doing Winc Academy exercises.",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
     url="https://github.com/WincAcademy/wincpy",
     classifiers=[
