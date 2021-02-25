f3ee71f2ba614129bfce4d67bb1df8292234254d
Thomas <thomas@wincacademy.nl>
2021-02-25 14:11:53 +0100

    Fixing some typos

--- wincpy/solutions/62311a1767294e058dc13c953e8690a4/main.py
+++ wincpy/solutions/62311a1767294e058dc13c953e8690a4/main.py
@@ -7,12 +7,12 @@ leek_price = 2
 print("Leek is " + str(leek_price) + " euro per kilo.")
 
 order = "leek 4"
-order_amount = int(order[order.find(" ") :])
+order_amount = int(order[order.find(" "):])
 sum_total = leek_price * order_amount
 print(sum_total)
 
 broccoli_price = 2.34
 order = "broccoli 1.6"
-order_amount = float(order[order.find(" ") :])
+order_amount = float(order[order.find(" "):])
 sum_total = round(broccoli_price * order_amount, 2)
 print(str(order_amount) + "kg broccoli costs " + str(sum_total) + "e")
--- wincpy/solutions/c545bc87620d4ced81cbddb8a90b4a51/main.py
+++ wincpy/solutions/c545bc87620d4ced81cbddb8a90b4a51/main.py
@@ -3,6 +3,9 @@ from helpers import get_countries
 __winc_id__ = 'c545bc87620d4ced81cbddb8a90b4a51'
 __human_name__ = 'for'
 
+
+
+
 def shortest_names(countries):
     shortest_len = float('inf')
     shortest_names = []
