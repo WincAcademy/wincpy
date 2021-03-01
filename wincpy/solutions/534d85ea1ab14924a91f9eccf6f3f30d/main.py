322a95985c32c9526ae321a0479e55d0d9d7e4b2
Stefan Wijnja (stfwn) <stefan@stfwn.com>
2021-03-01 11:27:23 +0000

    Improve formatting

--- wincpy/solutions/534d85ea1ab14924a91f9eccf6f3f30d/main.py
+++ wincpy/solutions/534d85ea1ab14924a91f9eccf6f3f30d/main.py
@@ -1,15 +1,16 @@
-import os
-
 # Do not modify these lines
 __winc_id__ = '534d85ea1ab14924a91f9eccf6f3f30d'
 __human_name__ = 'errors'
+
 
 # Your own code for testing your functions here
 def main():
     pass
 
+
 """Change the three functions below from Look Before You Leap (LBYL) to Easier
 to Ask for Forgiveness than Permission (EAFP)"""
+
 
 # Returns the addition of x and y if it's defined, otherwise returns 0
 def add(x, y):
@@ -27,12 +28,14 @@ def read_file(filename):
     except FileNotFoundError:
         return ''
 
+
 # Returns item at `index` from list `l` if possible, otherwise returns None
 def get_item_from_list(l, index):
     try:
         return l[index]
     except IndexError:
         return None
+
 
 if __name__ == '__main__':
     main()
