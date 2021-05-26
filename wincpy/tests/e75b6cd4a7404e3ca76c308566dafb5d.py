90073b0a96b4b4c4006f57b0783b7de1a497629b
Flip van Wijk <flip.vanwijk@gmail.com>
2021-05-26 11:47:20 +0200

    Fix it maybe

--- wincpy/tests/e75b6cd4a7404e3ca76c308566dafb5d.py
+++ wincpy/tests/e75b6cd4a7404e3ca76c308566dafb5d.py
@@ -1,3 +1,8 @@
+""" template.py
+
+Template for writing tests. This is just a file for convenience and has no
+importance beyond it."""
+
 from wincpy.helpers import compare_states, exec_assignment_code, get_main_abspath
 
 __winc_id__ = 'e75b6cd4a7404e3ca76c308566dafb5d'
