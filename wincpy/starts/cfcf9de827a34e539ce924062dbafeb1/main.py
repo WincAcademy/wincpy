344fb7c406fef39da8709e9acc087aef313e8190
Flip van Wijk <flip.vanwijk@gmail.com>
2021-06-08 16:28:31 +0200

    Add import statements necessary for skeleton

--- wincpy/starts/cfcf9de827a34e539ce924062dbafeb1/main.py
+++ wincpy/starts/cfcf9de827a34e539ce924062dbafeb1/main.py
@@ -4,6 +4,11 @@ __winc_id__ = 'cfcf9de827a34e539ce924062dbafeb1'
 __human_name__ = 'spa'
 
 # Implement your program below.
+from flask import Flask, request, jsonify, Response, render_template
+import helpers
+
+
+app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
