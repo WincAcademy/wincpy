f732ab2241d1453774ec0faf8e68362a4f66446e
Stefan Wijnja (stfwn) <stefan@stfwn.com>
2021-07-12 10:58:56 +0100

    Don't use --user for wincpy update

--- wincpy/main.py
+++ wincpy/main.py
@@ -12,9 +12,9 @@ def main(stdout, stderr):
     args = helpers.parse_args()
     ui.print_intro()
 
-    if args.action == 'start':
+    if args.action == "start":
         start(args)
-    elif args.action == 'check':
+    elif args.action == "check":
         result = check(args)
         ui.report_check_result(result)
         if all([score for _, score in result]):
@@ -22,26 +22,26 @@ def main(stdout, stderr):
         else:
             # No runtime errors but the solution didn't pass.
             sys.exit(2)
-    elif args.action == 'update':
+    elif args.action == "update":
         update()
-    elif args.action == 'solve':
+    elif args.action == "solve":
         result = check(args)
         passed = all([x for _, x in result])
         if passed:
             solve(args)
         else:
-            ui.report_error('solve_first')
+            ui.report_error("solve_first")
             sys.exit(1)
 
 
 def start(args):
     iddb = helpers.get_iddb()
     if args.winc_id not in iddb:
-        ui.report_error('unknown_winc_id')
+        ui.report_error("unknown_winc_id")
         sys.exit(1)
 
-    human_name = iddb[args.winc_id]['human_name']
-    ui.report_neutral('assignment_start', assignment_name=human_name)
+    human_name = iddb[args.winc_id]["human_name"]
+    ui.report_neutral("assignment_start", assignment_name=human_name)
 
     starts_abspath = starts.__path__[0]
     starts_dirs = list(os.walk(starts_abspath))[0][1]
@@ -52,20 +52,22 @@ def start(args):
         try:
             os.mkdir(human_name)
         except FileExistsError:
-            ui.report_error('dir_exists', dirname=human_name)
+            ui.report_error("dir_exists", dirname=human_name)
             sys.exit(1)
-        with open(os.path.join(human_name, 'main.py'), 'w') as fp:
-            fp.write('# Do not modify these lines\n'
-                     + f"__winc_id__ = '{args.winc_id}'\n"
-                     + f"__human_name__ = '{human_name}'\n\n"
-                     + '# Add your code after this line\n')
+        with open(os.path.join(human_name, "main.py"), "w") as fp:
+            fp.write(
+                "# Do not modify these lines\n"
+                + f"__winc_id__ = '{args.winc_id}'\n"
+                + f"__human_name__ = '{human_name}'\n\n"
+                + "# Add your code after this line\n"
+            )
     else:
         try:
             shutil.copytree(starts_dirs[args.winc_id], human_name)
         except FileExistsError:
-            ui.report_error('dir_exists', dirname=human_name)
+            ui.report_error("dir_exists", dirname=human_name)
             sys.exit(1)
-    ui.report_success('assignment_start', assignment_name=human_name)
+    ui.report_success("assignment_start", assignment_name=human_name)
 
 
 def check(args):
@@ -73,10 +75,10 @@ def check(args):
 
     winc_id = student_module.__winc_id__
     try:
-        check = importlib.import_module(f'.{winc_id}', 'wincpy.checks')
+        check = importlib.import_module(f".{winc_id}", "wincpy.checks")
         # solution_module = importlib.import_module(f'.{winc_id}', 'wincpy.solutions')
     except ImportError:
-        ui.report_error('no_check_found', assignment_name=student_module.__human_name__)
+        ui.report_error("no_check_found", assignment_name=student_module.__human_name__)
         sys.exit(1)
 
     # result = test.run(student_module, solution_module)
@@ -86,9 +88,8 @@ def check(args):
 
 
 def update():
-    release_url = 'git+https://github.com/WincAcademy/wincpy@release'
-    subprocess.run(['pip', 'install', release_url, '--user', '--upgrade'],
-                   check=True)
+    release_url = "git+https://github.com/WincAcademy/wincpy@release"
+    subprocess.run(["pip", "install", release_url, "--upgrade"], check=True)
 
 
 def solve(args):
@@ -98,14 +99,15 @@ def solve(args):
     solutions_abspath = solutions.__path__[0]
     solution_abspath = os.path.join(solutions_abspath, winc_id)
     if not os.path.isdir(solution_abspath):
-        ui.report_error('no_solution_available',
-                        exercise_name=student_module.__human_name__)
+        ui.report_error(
+            "no_solution_available", exercise_name=student_module.__human_name__
+        )
         sys.exit(1)
 
     try:
-        dest_dir = student_module.__human_name__ + '_example_solution'
+        dest_dir = student_module.__human_name__ + "_example_solution"
         shutil.copytree(solution_abspath, dest_dir)
-        ui.report_success('solution_available', solution_dir=dest_dir)
+        ui.report_success("solution_available", solution_dir=dest_dir)
     except:
-        ui.report_error('dir_exists', dirname=dest_dir)
+        ui.report_error("dir_exists", dirname=dest_dir)
         sys.exit(1)
