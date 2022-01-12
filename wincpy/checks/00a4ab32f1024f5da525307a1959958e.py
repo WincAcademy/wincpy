ff8d8f4fcac74c5f4513957af33dd92e5b0f6b1d
Niels Bom <niels@nielsbom.com>
2022-01-12 13:46:33 +0100

    Add dictionaries v2 exercise
    Some courses don't feature loops before dictionaries, the old
    dictionaries exercise was hard to complete without loops. The logic was also
    quite difficult. We can replace the old one later.

---
+++ wincpy/checks/00a4ab32f1024f5da525307a1959958e.py
@@ -0,0 +1,94 @@
+__winc_id__ = "00a4ab32f1024f5da525307a1959958e"
+
+TEST_DATA = {
+    "name": "Hank Bobbiton",
+    "date_of_birth": "1980-12-31",
+    "place_of_birth": "Brussels",
+    "height": 178.52,
+    "nationality": "Belgium",
+}
+
+
+def check_create_passport(student_module):
+    """`create_passport` is implemented correctly"""
+    passport = student_module.create_passport(**TEST_DATA)
+    assert passport == TEST_DATA, "The returned dict is not as we expected it to be."
+
+
+def check_add_stamp(student_module):
+    """`add_stamp` is implemented correctly"""
+    passport = student_module.create_passport(**TEST_DATA)
+
+    nationality = TEST_DATA["nationality"]
+    assert nationality not in passport.get(
+        "stamps", {}
+    ), f"Did not expect to find {nationality} in stamps"
+
+    for country in ["Afghanistan", "Bulgaria"]:
+        assert country not in passport.get(
+            "stamps", {}
+        ), f"Did not expect to find {country} in the passport yet"
+        passport = student_module.add_stamp(passport, country)
+        assert (
+            country in passport["stamps"]
+        ), f"Expected to find {country} in the passport"
+
+
+def check_add_biometric_data(student_module):
+    """`add_biometric_data` is implemented correctly"""
+    passport = student_module.create_passport(**TEST_DATA)
+
+    passport = student_module.add_biometric_data(
+        passport, "eye_color_left", "blue", "2020-05-05"
+    )
+    assert "biometric" in passport, "Key `biometric` not found in passport."
+    assert (
+        "eye_color_left" in passport["biometric"]
+    ), "`add_biometric_data` did not add key for given biometric data type"
+    assert dict == type(
+        passport["biometric"]["eye_color_left"]
+    ), "No dictionary found for new biometric data."
+    assert (
+        "value" in passport["biometric"]["eye_color_left"]
+    ), '`add_biometric_data` did not create a "value" key for the given biometric data'
+
+    assert (
+        "date" in passport["biometric"]["eye_color_left"]
+    ), '`add_biometric_data` did not create a "date" key for the date of recording of the biometric data'
+
+    assert (
+        "blue" == passport["biometric"]["eye_color_left"]["value"]
+    ), "`add_biometric_data` did not set the correct value for the biometric data"
+
+    assert (
+        "2020-05-05" == passport["biometric"]["eye_color_left"]["date"]
+    ), "`add_biometric_data` did not set the correct date for the biometric data"
+
+    # Adding another measurement
+    passport = student_module.add_biometric_data(
+        passport, "eye_color_right", "brown", "2020-06-06"
+    )
+    assert 2 == len(
+        passport["biometric"]
+    ), "Adding more biometric data does not result in correct number of biometric data values."
+    assert (
+        {
+            "eye_color_left": {"date": "2020-05-05", "value": "blue"},
+            "eye_color_right": {"date": "2020-06-06", "value": "brown"},
+        }
+        == passport["biometric"],
+    ), "Adding more biometric data results in incorrect data on passport."
+
+    # Updating a measurement
+    passport = student_module.add_biometric_data(
+        passport, "eye_color_left", "green", "2022-01-12"
+    )
+    assert 2 == len(
+        passport["biometric"]
+    ), "Updating biometric data does not result in correct number of biometric data values."
+    assert {
+        "eye_color_left": {"date": "2022-01-12", "value": "green"},
+        "eye_color_right": {"date": "2020-06-06", "value": "brown"},
+    } == passport[
+        "biometric"
+    ], "Updating biometric data results in incorrect data on passport."
---
+++ wincpy/solutions/00a4ab32f1024f5da525307a1959958e/main.py
@@ -0,0 +1,69 @@
+from helpers import get_countries
+
+__winc_id__ = "00a4ab32f1024f5da525307a1959958e"
+__human_name__ = "dictionariesv2"
+
+
+def create_passport(name, date_of_birth, place_of_birth, height, nationality):
+    return {
+        "name": name,
+        "date_of_birth": date_of_birth,
+        "place_of_birth": place_of_birth,
+        "height": height,
+        "nationality": nationality,
+    }
+
+
+def add_stamp(passport, country):
+    if "stamps" not in passport:
+        passport["stamps"] = []
+
+    if country not in passport["stamps"]:
+        passport["stamps"].append(country)
+
+    return passport
+
+
+def add_biometric_data(passport, biometric_data_type, value, date):
+    if not "biometric" in passport:
+        passport["biometric"] = {}
+    biometric_data = {"date": date, "value": value}
+    passport["biometric"][biometric_data_type] = biometric_data
+    return passport
+
+
+if __name__ == "__main__":
+    # Part 1: Create passport
+    countries = get_countries()
+    hank = create_passport("Hank Bobbiton", "1980-12-31", "Brussels", 178.52, "Belgium")
+    print(hank)
+
+    # Part 2: Add stamp
+    hank = add_stamp(hank, countries[0])
+    hank = add_stamp(hank, countries[32])
+    print(hank)
+
+    # Part 3: Add biometric data
+    hank = add_biometric_data(hank, "eye_color_left", "blue", "2020-05-05")
+    hank = add_biometric_data(hank, "eye_color_right", "blue", "2020-05-05")
+    print(hank)
+
+    # Hank gets a new left eye because of an accident
+    hank = add_biometric_data(hank, "eye_color_left", "brown", "2022-01-10")
+    print(hank)
+
+    # Add fingerprints too: just another value, but this is also a dict.
+    fingerprint_data = {
+        "left_pinky": "2378945",
+        "left_ring": "2349081",
+        "left_middle": "132890",
+        "left_index": "9823234",
+        "left_thumb": "0924131",
+        "right_thumb": "6234923",
+        "right_index": "13249734",
+        "right_middle": "34023784",
+        "right_ring": "12332538",
+        "right_pinky": "32458970",
+    }
+    hank = add_biometric_data(hank, "finger_prints", fingerprint_data, "2022-01-12")
+    print(hank)
