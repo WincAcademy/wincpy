e55e57f0162fae08c2e7cc776f39b0d33fa2bc83
Winc Tooling <124043733+winctooling@users.noreply.github.com>
2025-04-22 12:05:57 +0200

    Add files via upload

---
+++ wincpy/starts/6eb355e1a60f48a28a0bbbd0c88d9ab4/main.py
@@ -0,0 +1,27 @@
+# Do not modify these lines
+__winc_id__ = "6eb355e1a60f48a28a0bbbd0c88d9ab4"
+__human_name__ = "lists"
+
+# Add your code after this line
+# 1 task
+
+film_names = [
+    "Valley of the dolls",
+    "Superman",
+    "The accidental tourist",
+    "Lincoln",
+    "Jaws",
+    "A",
+]
+
+
+# 2 task
+
+golden_globes = [
+    "jaws",
+    "star wars: episode iv - a new hope",
+    "e.t. the extra-terrestrial",
+    "memoirs of a geisha",
+]
+
+film_name = "Jaws"
